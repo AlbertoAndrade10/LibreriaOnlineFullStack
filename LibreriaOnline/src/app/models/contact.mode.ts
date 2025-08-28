ef262acb735eac4e85d57c035eb21fb05c61c20c
Alberto Andrade <albertoandradeortiz910@gmail.com>
2025-08-28 18:04:45 +0200

    Delete LibreriaOnline directory

--- LibreriaOnline/src/app/models/contact.mode.ts
+++
@@ -1,9 +0,0 @@
-export interface ContactInfo {
-    id: number;
-    type: 'linkedin' | 'infojobs' | 'email' | 'phone' | 'github' | 'website';
-    title: string;
-    value: string;
-    icon: string;
-    url?: string;
-    description?: string;
-}
