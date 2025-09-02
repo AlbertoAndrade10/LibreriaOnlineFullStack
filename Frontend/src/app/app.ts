429e68f254f20aad5443215d2f81b9d5797859c7
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-02 20:07:16 +0200

    Dependencies added and checked

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import AOS from 'aos';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Libreria Online');
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+  async ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const AOS = (await import('aos')).default;
+      AOS.init({
+        duration: 1000,
+        once: true,
+      });
+    }
+  }
 }
