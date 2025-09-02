178928ba7f683414056dd9b97c37f53f1f124a67
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-02 20:51:55 +0200

    CREATE: footer

--- Frontend/src/app/components/layouts/base-layout/base-layout.ts
+++ Frontend/src/app/components/layouts/base-layout/base-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../shared/header/header";
+import { Footer } from "../../shared/footer/footer";
 
 @Component({
   selector: 'app-base-layout',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './base-layout.html',
   styleUrl: './base-layout.css'
 })
