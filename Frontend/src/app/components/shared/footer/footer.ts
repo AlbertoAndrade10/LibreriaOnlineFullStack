fee02ea7786884d0d3d43e166c91aac1976a9410
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-02 21:48:57 +0200

    BUG: error routes fixed

--- Frontend/src/app/components/shared/footer/footer.ts
+++ Frontend/src/app/components/shared/footer/footer.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
