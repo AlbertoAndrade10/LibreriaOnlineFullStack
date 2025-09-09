0271883669939c266651995c34ed6f3a7b2e9fbb
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-09 15:43:05 +0200

    BACKEND: CREATE: DTOs y validaciones

---
+++ Backend/BookService/src/main/java/com/porfolio/BookService/dtos/BookCreateDTO.java
@@ -0,0 +1,34 @@
+package com.porfolio.BookService.dtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+public class BookCreateDTO {
+
+    @NotBlank(message = "The book name cannot be empty")
+    private String bookName;
+
+    @NotNull(message = "The author is mandatory")
+    private Long authorId;
+
+    @NotNull(message = "The literary genre is mandatory")
+    private Long literaryGenreId;
+
+    private String urlImage;
+
+    @NotNull(message = " The price is mandatory")
+    @Positive(message = "The price must be greather than 0")
+    private BigDecimal price;
+
+    private String description;
+
+    @NotNull(message = "The stock is mandatory")
+    @PositiveOrZero(message = "The stock cannot be negative")
+    private Integer stock;
+}
---
+++ Backend/BookService/src/main/java/com/porfolio/BookService/dtos/BookUpdateDTO.java
@@ -0,0 +1,24 @@
+package com.porfolio.BookService.dtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+public class BookUpdateDTO {
+
+    private String bookName;
+    private Long authorId;
+    private Long literaryGenreId;
+    private String urlImage;
+
+    @Positive(message = "The price must be greather than 0")
+    private BigDecimal price;
+
+    private String description;
+
+    @PositiveOrZero(message = "The stock cannot be negative")
+    private Integer stock;
+}
