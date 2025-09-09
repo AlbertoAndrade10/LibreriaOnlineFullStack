41ecebf5520571d8da04677bb02d7fe5e1cbf89b
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-09 16:07:25 +0200

    BACKEND: CREATE: AuthorRepository methods

--- Backend/BookService/src/main/java/com/porfolio/BookService/repositories/AuthorRepository.java
+++ Backend/BookService/src/main/java/com/porfolio/BookService/repositories/AuthorRepository.java
@@ -1,9 +1,16 @@
 package com.porfolio.BookService.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.porfolio.BookService.models.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    Optional<Author> findByAuthorName(String authorName);
+
+    List<Author> findByAuthorNameContainingIgnoreCase(String authorName);
 
 }
