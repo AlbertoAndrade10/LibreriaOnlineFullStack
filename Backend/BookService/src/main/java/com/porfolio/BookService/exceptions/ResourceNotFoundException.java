5f666d3253dbf95549c12c5365c4c75f3187c966
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-09 16:18:31 +0200

    BACKEND: CREATE: Excepciones personalizadas

---
+++ Backend/BookService/src/main/java/com/porfolio/BookService/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.porfolio.BookService.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resource, Long id) {
+        super(String.format("%s con ID %d no encontrado", resource, id));
+    }
+
+}
