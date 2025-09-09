efa7ca12b6839f87fe7ba9bf4f0b87895e5ab158
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-09 15:33:22 +0200

    BACKEND: Configuradas las relaciones entre los modelos de la base de datos

--- Backend/BookService/src/main/java/com/porfolio/BookService/models/Author.java
+++ Backend/BookService/src/main/java/com/porfolio/BookService/models/Author.java
@@ -1,12 +1,29 @@
 package com.porfolio.BookService.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "authors")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String authorName;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Book> books = new ArrayList<>();
 }
--- Backend/BookService/src/main/java/com/porfolio/BookService/models/Book.java
+++ Backend/BookService/src/main/java/com/porfolio/BookService/models/Book.java
@@ -3,16 +3,34 @@ package com.porfolio.BookService.models;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String bookName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
     private Author author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "literary_genre_id")
     private LiteraryGenre literaryGenre;
+
     private String urlImage;
     private BigDecimal price;
     private String description;
