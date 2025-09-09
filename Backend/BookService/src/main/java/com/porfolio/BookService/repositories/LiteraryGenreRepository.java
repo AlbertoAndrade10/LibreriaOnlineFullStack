bf95262534713a275d5fa7847a6f365bd8484a50
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-09 16:08:55 +0200

    BACKEND: CREATE: LiteraryGenreRepository methods

--- Backend/BookService/src/main/java/com/porfolio/BookService/repositories/LiteraryGenreRepository.java
+++ Backend/BookService/src/main/java/com/porfolio/BookService/repositories/LiteraryGenreRepository.java
@@ -1,9 +1,12 @@
 package com.porfolio.BookService.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.porfolio.BookService.models.LiteraryGenre;
 
 public interface LiteraryGenreRepository extends JpaRepository<LiteraryGenre, Long> {
 
+    Optional<LiteraryGenre> findByLiteraryGenreName(String genreName);
 }
