03f16de2c570a6e9d9d500b201c3b78c449314b1
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-10 19:50:33 +0200

    FRONTEND: Creado LoadingInterceptor

---
+++ Frontend/src/app/interceptors/LoadingInterceptor/loading-interceptor-interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  private totalRequests = 0;
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.totalRequests++;
+    // -->> CREAR UN COMPONENTE SPINNER PARA MOSTRARLO AQUI <<--
+
+    return next.handle(req).pipe(
+      finalize(() => {
+        this.totalRequests--;
+        if (this.totalRequests === 0) {
+          // -->> Ocultar loading indicator <<--
+        }
+      })
+    );
+  }
+}
