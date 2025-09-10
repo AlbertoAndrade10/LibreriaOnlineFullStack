6b104e0d161f37906768e0d6b307a8e7d7096917
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-10 20:23:00 +0200

    BACKEND: AuthService: BugFixed: Arreglado problema con getters LoginRequest.java

--- Backend/AuthService/src/main/java/com/porfolio/AuthService/request/LoginRequest.java
+++ Backend/AuthService/src/main/java/com/porfolio/AuthService/request/LoginRequest.java
@@ -8,4 +8,12 @@ public class LoginRequest {
     private String email;
     private String password;
 
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
 }
