1774185e4389b40b53ff4a60b1148ca55476ae51
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-15 20:50:37 +0200

    Updates databases

--- Backend/BookService/src/main/java/com/porfolio/BookService/dtos/AuthorDTO.java
+++ Backend/BookService/src/main/java/com/porfolio/BookService/dtos/AuthorDTO.java
@@ -1,26 +1,15 @@
 package com.porfolio.BookService.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorDTO {
 
     private Long id;
     private String authorName;
 
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getAuthorName() {
-        return this.authorName;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
 }
