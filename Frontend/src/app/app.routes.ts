e2c0b7296ffc5ac7f6e7f9f5b12624132c4738ec
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-17 13:26:07 +0200

    FRONTEND: Bugfixed: Error in routes fixed

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { BaseLayout } from './components/layouts/base-layout/base-layout';
 import { IndexPage } from './pages/index/index-page/index-page';
 import { BookPage } from './pages/book/book-page/book-page';
-import { ContactPage } from './pages/contact/contact-page/contact-page';
+
 
 
 export const routes: Routes = [
@@ -18,10 +18,7 @@ export const routes: Routes = [
                 path: 'books',
                 component: BookPage
             },
-            {
-                path: 'contact',
-                component: ContactPage
-            },
+
 
         ]
     }
