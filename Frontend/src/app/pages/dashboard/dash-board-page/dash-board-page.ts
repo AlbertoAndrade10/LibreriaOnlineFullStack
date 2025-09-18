eba45a1dc55278bcfc93b0af77c29388ff4d5733
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-18 08:58:48 +0200

    FRONTEND: Created DashBoardPage

---
+++ Frontend/src/app/pages/dashboard/dash-board-page/dash-board-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-board-page',
+  imports: [],
+  templateUrl: './dash-board-page.html',
+  styleUrl: './dash-board-page.css'
+})
+export class DashBoardPage {
+
+}
