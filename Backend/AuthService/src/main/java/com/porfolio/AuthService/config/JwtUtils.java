5967892bb11e9c8610205a75f692611a6c22f988
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-20 15:32:47 +0200

    BACKEND: AuthService -> JwtUtils created

---
+++ Backend/AuthService/src/main/java/com/porfolio/AuthService/config/JwtUtils.java
@@ -0,0 +1,36 @@
+package com.porfolio.AuthService.config;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    @SuppressWarnings("deprecation")
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    @SuppressWarnings("deprecation")
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+}
