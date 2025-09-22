79996d511543f45ce6b2de6fe4b487510f4615b7
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-09-22 09:57:14 +0200

    BACKEND: AuthService -> UserController updated

--- Backend/AuthService/src/main/java/com/porfolio/AuthService/controller/UserController.java
+++ Backend/AuthService/src/main/java/com/porfolio/AuthService/controller/UserController.java
@@ -1,5 +1,40 @@
 package com.porfolio.AuthService.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.porfolio.AuthService.entities.User;
+import com.porfolio.AuthService.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+
+    public ResponseEntity<List<User>> allUsers() {
+        List<User> users = userService.allUsers();
+
+        return ResponseEntity.ok(users);
+    }
 
 }
